ad1d981b5183a33cf355a57271dc0242a20c8411
user <ravpaluch@gmail.com>
2017-06-05 21:38:50 +0200

    Added sum and average to comp.

--- src/players.js
+++ src/players.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Table} from 'react-bootstrap'
 const players = [
-    {username:"Del Piero", points:10},
-    {username:"Zidane", points:20},
-    {username:"Beckham", points:20,},
-    {username:"Zambrotta", points:10},
-    {username: "Nedved",points:13 },
-    {username:"Bufon", points:0},
-    {username:"Terezegeut", points:1},
-    {username:"Ronaldo", points:2},
-    {username:"Carlos", points:22},
-    {username:"Mik", points:4}
+    {id:1,username:"Del Piero", points:10},
+    {id:2,username:"Zidane", points:20},
+    {id:3,username:"Beckham", points:20,},
+    {id:4,username:"Zambrotta", points:10},
+    {id:5,username: "Nedved",points:13 },
+    {id:6,username:"Bufon", points:0},
+    {id:7,username:"Terezegeut", points:1},
+    {id:8,username:"Ronaldo", points:2},
+    {id:9,username:"Carlos", points:220},
+    {id:10,username:"Mik", points:4}
     ];
 
 const Players = () => (
@@ -28,12 +28,20 @@ const Players = () => (
 <tbody>
 {players.map(
     player => (
-     <tr>
+     <tr key={player.id}>
     <td>{player.username}</td>
     <td>{player.points}</td>
-     </tr>
+     </tr>))}
 
-    ))}
+
+    <tr>
+        <td>Total score</td>
+        <td>{players.map((player)=>player.points).reduce((a,b)=>(a+b))}</td>
+    </tr>
+<tr><td>Average</td>
+    <td>{players.map((player)=>player.points).reduce((a,b)=>(a+b))/players.length}</td></tr>
+
+
 
 
 </tbody>
