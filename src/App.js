5d3d09137a8bf7f5d6c211edb10798a4a10a8e83
user <ravpaluch@gmail.com>
2017-06-06 21:32:43 +0200

    Marked players with specific value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
+import {Table} from 'react-bootstrap';
 
 import './App.css'
-import Players from'./players.js'
+
+import players from './players.js'
 
 class App extends Component {
   render() {
     return (
-        <Players/>
+        <div>
+          <h1>
+            Players
+          </h1>
+          <Table>
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+            </thead>
+            <tbody>
+            {players.map(
+                (player) => (
+                    <tr key={player.id}>
+                      <td style ={{color : player.points >100? 'red': player.points}}>{player.username}</td>
+                      <td style ={{color : player.points >100? 'red': player.points}}>{player.points}<td></td></td>
+                    </tr>))}
+
+
+            <tr>
+              <td>Total score</td>
+              <td>{players.map((player)=>player.points).reduce((a,b)=>(a+b))}</td>
+            </tr>
+            <tr><td>Average</td>
+              <td>{players.map((player)=>player.points).reduce((a,b)=>(a+b))/players.length}</td></tr>
+
+
+
+
+            </tbody>
+          </Table>
+
+
+
+
+        </div>
+
 
     );
   }
--- src/players.js
+++ src/players.js
@@ -13,45 +13,10 @@ const players = [
     {id:10,username:"Mik", points:4}
     ];
 
-const Players = () => (
-    <div>
-        <h1>
-            Players
-        </h1>
-        <Table>
-            <thead>
-            <tr>
-            <th>Name</th>
-            <th>Score</th>
-            </tr>
-            </thead>
-<tbody>
-{players.map(
-    player => (
-     <tr key={player.id}>
-    <td>{player.username}</td>
-    <td>{player.points}</td>
-     </tr>))}
-
-
-    <tr>
-        <td>Total score</td>
-        <td>{players.map((player)=>player.points).reduce((a,b)=>(a+b))}</td>
-    </tr>
-<tr><td>Average</td>
-    <td>{players.map((player)=>player.points).reduce((a,b)=>(a+b))/players.length}</td></tr>
 
 
 
-
-</tbody>
-        </Table>
-    </div>
-)
-
-
-export default Players;
-
+export default players;
 
 
 
